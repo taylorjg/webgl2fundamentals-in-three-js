f2ab05b089bbfa75030c3c93e23c32b8cf3cc438
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-07-01 20:14:47 +0100

    Work on ex2 of How It Works

--- src/how-it-works-ex2.js
+++ src/how-it-works-ex2.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three'
-import * as dat from 'dat.gui'
+import * as THREE from "three"
+import * as dat from "dat.gui"
 
 const vertexShader = `
 out vec4 v_color;
@@ -8,12 +8,8 @@ void main() {
   // Multiply the position by the matrix.
   // gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  gl_Position.z = 0.0;
 
-  // Convert from clipspace to colorspace.
-  // Clipspace goes -1.0 to +1.0
-  // Colorspace goes from 0.0 to 1.0
-  v_color = gl_Position * 0.5 + 0.5;
+  v_color = color;
 }
 `
 
@@ -30,7 +26,7 @@ void main() {
 `
 
 const main = () => {
-  const container = document.getElementById('container')
+  const container = document.getElementById("container")
   const width = container.clientWidth
   const height = container.clientHeight
 
@@ -46,17 +42,40 @@ const main = () => {
   const camera = new THREE.OrthographicCamera(0, width, height, 0, 0, 1)
 
   const points = [
-    [0, -100],
-    [150, 125],
-    [-175, 100]
+    [-150, -100],
+    [150, -100],
+    [-150, 100],
+    // NOTE: change the winding order of the second triangle to prevent it from being culled
+    [150, 100],
+    [-150, 100],
+    [150, -100]
+  ]
+
+  const r1 = Math.random()
+  const g1 = Math.random()
+  const b1 = Math.random()
+  const r2 = Math.random()
+  const g2 = Math.random()
+  const b2 = Math.random()
+
+  const colors = [
+    r1, g1, b1, 1,
+    r1, g1, b1, 1,
+    r1, g1, b1, 1,
+    r2, g2, b2, 1,
+    r2, g2, b2, 1,
+    r2, g2, b2, 1
   ]
 
   const geometry = new THREE.BufferGeometry()
   geometry.setFromPoints(points.map(([x, y]) => ({ x, y })))
+  geometry.setAttribute("color", new THREE.BufferAttribute(new Float32Array(colors), 4))
+  console.log(geometry)
   const material = new THREE.ShaderMaterial({
     glslVersion: THREE.GLSL3,
     vertexShader,
-    fragmentShader
+    fragmentShader,
+    vertexColors: true
   })
   const mesh = new THREE.Mesh(geometry, material)
   mesh.position.x = 200
@@ -91,19 +110,19 @@ const main = () => {
     }
 
   const params = {
-    'x': mesh.position.x,
-    'y': mesh.position.y,
-    'angle': THREE.MathUtils.radToDeg(mesh.rotation.z),
-    'scaleX': mesh.scale.x,
-    'scaleY': mesh.scale.y,
+    "x": mesh.position.x,
+    "y": mesh.position.y,
+    "angle": THREE.MathUtils.radToDeg(mesh.rotation.z),
+    "scaleX": mesh.scale.x,
+    "scaleY": mesh.scale.y,
   }
 
   const gui = new dat.GUI()
-  gui.add(params, 'x', 0, width, 1).onChange(updatePosition(0))
-  gui.add(params, 'y', 0, height, 1).onChange(updatePosition(1))
-  gui.add(params, 'angle', 0, 360, 1).onChange(updateAngle)
-  gui.add(params, 'scaleX', -5, 5, 0.01).onChange(updateScale(0))
-  gui.add(params, 'scaleY', -5, 5, 0.01).onChange(updateScale(1))
+  gui.add(params, "x", 0, width, 1).onChange(updatePosition(0))
+  gui.add(params, "y", 0, height, 1).onChange(updatePosition(1))
+  gui.add(params, "angle", 0, 360, 1).onChange(updateAngle)
+  gui.add(params, "scaleX", -5, 5, 0.01).onChange(updateScale(0))
+  gui.add(params, "scaleY", -5, 5, 0.01).onChange(updateScale(1))
   gui.open()
 
   render()
